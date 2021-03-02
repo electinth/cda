cfd863e5de4e60e2d8d9f6a2e8e5cb81fbfdee78
Th1nkK1D <witheep@gmail.com>
2021-03-02 12:11:53 +0700

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+const { tailwindExtractor } = require("tailwindcss/lib/lib/purgeUnusedStyles");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  purge: {
+    mode: "all",
+    content: ["./src/**/*.svelte", "./src/**/*.html"],
+    options: {
+      defaultExtractor: (content) => [
+        ...tailwindExtractor(content),
+        // Match Svelte class: directives (https://github.com/tailwindlabs/tailwindcss/discussions/1731)
+        ...[...content.matchAll(/(?:class:)*([\w\d-/:%.]+)/gm)].map(
+          ([_match, group, ..._rest]) => group
+        ),
+      ],
+      keyframes: true,
+    },
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      heading: ["Kondolar Thai", ...defaultTheme.fontFamily.serif],
+      subtitle: ["Anuphan", ...defaultTheme.fontFamily.sans],
+      body: ["Bai Jamjuri", ...defaultTheme.fontFamily.sans],
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
