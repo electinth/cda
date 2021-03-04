7d3bdc5ae125f42e96cbccbfb7fe61b0c83bd435
Th1nkK1D <witheep@gmail.com>
2021-03-04 14:51:53 +0700

    Update common package, add nav bar with mock about page

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,8 @@
 import * as sapper from '@sapper/app';
-import { Footer } from '@electinth/common';
+import { Footer, NavigationBar } from '@electinth/common/';
 
 customElements.define('elect-footer', Footer);
+customElements.define('navigation-bar', NavigationBar);
 
 sapper.start({
   target: document.querySelector('#sapper'),
