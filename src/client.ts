56bbdadd99572db88630647dda200a43fedf8f1e
Th1nkK1D <witheep@gmail.com>
2021-03-22 18:56:59 +0700

    Add staging banner

--- src/client.ts
+++ src/client.ts
@@ -1,8 +1,9 @@
 import * as sapper from '@sapper/app';
-import { Footer, NavigationBar } from '@electinth/common/';
+import { Footer, NavigationBar, StagingBanner } from '@electinth/common/';
 
 customElements.define('elect-footer', Footer);
 customElements.define('navigation-bar', NavigationBar);
+customElements.define('staging-banner', StagingBanner);
 
 sapper.start({
   target: document.querySelector('#sapper'),
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,15 +1,23 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
+
   import Nav from '../components/nav.svelte';
 
   export let segment: string;
+  let showStagingBanner = false;
+
+  onMount(() => {
+    showStagingBanner = window.location.hostname.includes('github');
+  });
 
   $: activePath = segment || '';
 </script>
 
 <div class="flex flex-col min-h-screen">
+  {#if showStagingBanner}
+    <staging-banner />
+  {/if}
   <Nav {activePath} />
-
   <slot />
-
   <elect-footer />
 </div>
