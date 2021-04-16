89c98e8080acc6df5aee8c280cbaedb5c563a3d3
Th1nkK1D <Th1nkK1D@users.noreply.github.com>
2021-04-16 12:02:09 +0000

    deploy: 3d014ffe1efb82e14b969b968bf07b022a18243c

---
+++ client/about.58907715.js
@@ -0,0 +1,1 @@
+import{S as s,i as a,s as t,e,t as o,c as r,a as c,b as n,d as u,h as i,j as l,n as p}from"./client.17f64136.js";function d(s){let a,t;return{c(){a=e("div"),t=o("About page")},l(s){a=r(s,"DIV",{});var e=c(a);t=n(e,"About page"),e.forEach(u)},m(s,e){i(s,a,e),l(a,t)},p:p,i:p,o:p,d(s){s&&u(a)}}}export default class extends s{constructor(s){super(),a(this,s,null,d,t,{})}}
