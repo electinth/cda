587b96280ee581bf5a4230bc0caf778dfb328f91
thasarito <thasarito@users.noreply.github.com>
2021-04-20 13:47:37 +0000

    deploy: 538991647e380b41c095847d174a33223c67f0d6

---
+++ client/about.1fa046ff.js
@@ -0,0 +1,1 @@
+import{S as s,i as a,s as t,e,t as o,c as r,a as c,b as n,d as u,h as i,j as l,n as b}from"./client.019bcbb6.js";function p(s){let a,t;return{c(){a=e("div"),t=o("About page")},l(s){a=r(s,"DIV",{});var e=c(a);t=n(e,"About page"),e.forEach(u)},m(s,e){i(s,a,e),l(a,t)},p:b,i:b,o:b,d(s){s&&u(a)}}}export default class extends s{constructor(s){super(),a(this,s,null,p,t,{})}}
