41fe8130ce718a032537d6a128011c213c810213
Th1nkK1D <witheep@gmail.com>
2021-04-20 10:45:27 +0700

    Refactor individual space template

---
+++ src/components/visualizations/individual-space-template.svelte
@@ -0,0 +1,128 @@
+<script lang="ts" context="module">
+  export interface MembersData {
+    index: number;
+    years: string[];
+    name: string;
+    description: string;
+    image: string;
+    color: Color;
+  }
+</script>
+
+<script lang="ts">
+  import type { Color } from 'three';
+  import { CDA_COUNTS, YEARS } from '../../utils/stats';
+  import { pickNumbersBetween } from '../../utils/gaussian';
+  import CdaSpace from '../cda-space/cda-space.svelte';
+  import type { Sphere } from '../../utils/three/sphere';
+  import InfoHead from '../cda-space/info-dialog/info-head.svelte';
+  import GroupBox from '../cda-space/info-dialog/group-box.svelte';
+  import MemberRow from '../cda-space/info-dialog/member-row.svelte';
+  import SubgroupBox from '../cda-space/info-dialog/subgroup-box.svelte';
+  import ControlButton from '../cda-space/info-dialog/control-button.svelte';
+  import type { ControlState } from '../cda-space/info-dialog/control-button.svelte';
+
+  interface IndividualMemberNodeData {
+    index: number;
+    number: number;
+    name: string;
+    image: string;
+  }
+
+  export let membersData: MembersData[];
+
+  const data = YEARS.map((year) => {
+    const membersInThisYear = membersData.filter((member) =>
+      member.years.includes(year)
+    );
+    const membersPosition = pickNumbersBetween(
+      0,
+      CDA_COUNTS[year],
+      membersInThisYear.length
+    );
+
+    const nodes = new Array(CDA_COUNTS[year]).fill({});
+
+    membersInThisYear.forEach(({ color, index, name }, indexInThisYear) => {
+      nodes[membersPosition[indexInThisYear]] = {
+        primaryColor: color,
+        data: {
+          index,
+          number: index + 1,
+          name: name,
+        } as IndividualMemberNodeData,
+        group: `${index}`,
+        isIndividual: true,
+      };
+    });
+
+    return nodes;
+  });
+
+  let nodes: Sphere<IndividualMemberNodeData>[];
+  let selectedNodes: Sphere<IndividualMemberNodeData>[];
+  let isGroupBoxOpened = false;
+
+  const onMemberSelected = (index: number) => {
+    selectedNodes = nodes.filter(({ data }) => data?.index === index);
+  };
+
+  const onControlTriggered = () => {
+    switch (state) {
+      case 'hidden':
+        isGroupBoxOpened = true;
+        break;
+      case 'expanded':
+        isGroupBoxOpened = false;
+        break;
+      case 'selected':
+        selectedNodes = [];
+        break;
+    }
+  };
+
+  $: state = (isGroupBoxOpened
+    ? selectedNodes?.length > 0
+      ? 'selected'
+      : 'expanded'
+    : 'hidden') as ControlState;
+
+  $: displayMembers = isGroupBoxOpened
+    ? selectedNodes && selectedNodes.length > 0
+      ? [membersData.find(({ index }) => selectedNodes[0].data.index == index)]
+      : membersData
+    : [];
+
+  $: {
+    if (selectedNodes && selectedNodes.length > 0) {
+      isGroupBoxOpened = true;
+    }
+  }
+</script>
+
+<CdaSpace {data} bind:nodes bind:selectedNodes isFreeze={isGroupBoxOpened}>
+  <div class="flex justify-end">
+    <ControlButton {state} on:click={() => onControlTriggered()} />
+  </div>
+  <div class="relative flex flex-col" class:space-y-2={isGroupBoxOpened}>
+    <InfoHead
+      class="flex flex-row space-x-2 z-20"
+      on:click={() => onControlTriggered()}
+    >
+      <div class="flex-1 my-auto">สสร. มากกว่า 1 ครั้ง</div>
+      <div class="text-h4 font-bold">{membersData.length}</div>
+      <div class="my-auto">คน</div>
+    </InfoHead>
+    <GroupBox
+      class="space-y-2 {isGroupBoxOpened
+        ? ''
+        : 'absolute inset-0 transform translate-x-2 translate-y-2'}"
+    >
+      {#each displayMembers as { index, color, name, description }}
+        <SubgroupBox on:click={() => onMemberSelected(index)}>
+          <MemberRow {color} {name} {description} number={index + 1} />
+        </SubgroupBox>
+      {/each}
+    </GroupBox>
+  </div>
+</CdaSpace>
--- src/components/visualizations/multipleyear.svelte
+++ src/components/visualizations/multipleyear.svelte
@@ -1,23 +1,8 @@
 <script lang="ts">
   import { Color } from 'three';
-  import { CDA_COUNTS, YEARS } from '../../utils/stats';
   import multipleyearMembers from '../../data/multipleyear-members.csv';
-  import { pickNumbersBetween } from '../../utils/gaussian';
-  import CdaSpace from '../cda-space/cda-space.svelte';
-  import type { Sphere } from '../../utils/three/sphere';
-  import InfoHead from '../cda-space/info-dialog/info-head.svelte';
-  import GroupBox from '../cda-space/info-dialog/group-box.svelte';
-  import MemberRow from '../cda-space/info-dialog/member-row.svelte';
-  import SubgroupBox from '../cda-space/info-dialog/subgroup-box.svelte';
-  import ControlButton from '../cda-space/info-dialog/control-button.svelte';
-  import type { ControlState } from '../cda-space/info-dialog/control-button.svelte';
-
-  interface MultipleyearNodeData {
-    index: number;
-    number: number;
-    image: string;
-    name: string;
-  }
+  import IndividualSpaceTemplate from './individual-space-template.svelte';
+  import type { MembersData } from './individual-space-template.svelte';
 
   const dataSphereColors = [
     new Color('#0066FF'),
@@ -27,111 +12,20 @@
     new Color('#C86FFF'),
   ];
 
-  const membersData = multipleyearMembers.map(({ year, ...rest }, index) => ({
-    index,
-    year: year.split(' '),
-    color: dataSphereColors[index],
-    ...rest,
-  }));
+  const membersData: MembersData[] = multipleyearMembers.map(
+    ({ year, ...rest }, index) => {
+      const years = year.split(' ');
 
-  const data = YEARS.map((year) => {
-    const membersInThisYear = membersData.filter((member) =>
-      member.year.includes(year)
-    );
-    const membersPosition = pickNumbersBetween(
-      0,
-      CDA_COUNTS[year],
-      membersInThisYear.length
-    );
-
-    const nodes = new Array(CDA_COUNTS[year]).fill({});
-
-    membersInThisYear.forEach(({ color, index, name }, indexInThisYear) => {
-      nodes[membersPosition[indexInThisYear]] = {
-        primaryColor: color,
-        data: {
-          index,
-          number: index + 1,
-          image: 'https://place-hold.it/70',
-          name: name,
-        } as MultipleyearNodeData,
-        group: `${index}`,
-        isIndividual: true,
+      return {
+        index,
+        years,
+        color: dataSphereColors[index],
+        description: `เป็นสมาชิกของสภาร่างรัฐธรรมนูญ พ.ศ. ${year[0]} และสภาร่างรัฐธรรมนูญ พ.ศ. ${year[1]}`,
+        image: 'https://place-hold.it/70',
+        ...rest,
       };
-    });
-
-    return nodes;
-  });
-
-  let nodes: Sphere<MultipleyearNodeData>[];
-  let selectedNodes: Sphere<MultipleyearNodeData>[];
-  let isGroupBoxOpened = false;
-
-  const onMemberSelected = (index: number) => {
-    selectedNodes = nodes.filter(({ data }) => data?.index === index);
-  };
-
-  const onControlTriggered = () => {
-    switch (state) {
-      case 'hidden':
-        isGroupBoxOpened = true;
-        break;
-      case 'expanded':
-        isGroupBoxOpened = false;
-        break;
-      case 'selected':
-        selectedNodes = [];
-        break;
     }
-  };
-
-  $: state = isGroupBoxOpened
-    ? selectedNodes?.length > 0
-      ? 'selected'
-      : 'expanded'
-    : ('hidden' as ControlState);
-
-  $: displayMembers = isGroupBoxOpened
-    ? selectedNodes && selectedNodes.length > 0
-      ? [membersData.find(({ index }) => selectedNodes[0].data.index == index)]
-      : membersData
-    : [];
-
-  $: {
-    if (selectedNodes && selectedNodes.length > 0) {
-      isGroupBoxOpened = true;
-    }
-  }
+  );
 </script>
 
-<CdaSpace {data} bind:nodes bind:selectedNodes isFreeze={isGroupBoxOpened}>
-  <div class="flex justify-end">
-    <ControlButton {state} on:click={() => onControlTriggered()} />
-  </div>
-  <div class="relative flex flex-col" class:space-y-2={isGroupBoxOpened}>
-    <InfoHead
-      class="flex flex-row space-x-2 z-20"
-      on:click={() => onControlTriggered()}
-    >
-      <div class="flex-1 my-auto">สสร. มากกว่า 1 ครั้ง</div>
-      <div class="text-h4 font-bold">{membersData.length}</div>
-      <div class="my-auto">คน</div>
-    </InfoHead>
-    <GroupBox
-      class="space-y-2 {isGroupBoxOpened
-        ? ''
-        : 'absolute inset-0 transform translate-x-2 translate-y-2'}"
-    >
-      {#each displayMembers as { name, color, index, year }}
-        <SubgroupBox on:click={() => onMemberSelected(index)}>
-          <MemberRow
-            {name}
-            {color}
-            number={index + 1}
-            description="เป็นสมาชิกของสภาร่างรัฐธรรมนูญ พ.ศ. {year[0]} และสภาร่างรัฐธรรมนูญ พ.ศ. {year[1]}"
-          />
-        </SubgroupBox>
-      {/each}
-    </GroupBox>
-  </div>
-</CdaSpace>
+<IndividualSpaceTemplate {membersData} />
