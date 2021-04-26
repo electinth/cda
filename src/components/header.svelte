ba51ede13b8c1d2346198c1ad87996246171312a
Th1nkK1D <witheep@gmail.com>
2021-04-26 15:47:01 +0700

    Update header spacing

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -2,16 +2,16 @@
   export let number = 0;
 </script>
 
-<h2
+<div
   class="max-w-4xl flex flex-col justify-center items-center mx-auto my-9 text-center"
 >
   <div class="font-semibold text-body1 border-b-2 border-black pb-1">
     หัวข้อที่ {number}
   </div>
-  <div class="font-semibold text-h3 my-2">
+  <h2 class="font-semibold text-h3 my-3">
     <slot name="topic" />
-  </div>
-  <div class="text-body1">
+  </h2>
+  <p class="text-body1">
     <slot name="description" />
-  </div>
-</h2>
+  </p>
+</div>
