f97274d35c36f8e2b85c20050222655002fb5b7c
Th1nkK1D <witheep@gmail.com>
2021-04-27 17:19:51 +0700

    Update bar viz data

---
+++ src/components/link-external.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let href: string;
+</script>
+
+<a
+  {href}
+  target="_blank"
+  rel="noopener noreferrer"
+  class="break-words hover:underline"
+>
+  <slot />
+</a>
